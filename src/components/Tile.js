a958d03e581dfbe9f1fdcf2d8c510a6365f0c755
YY T <changiyy07@gmail.com>
2024-05-25 11:55:27 +0800

    Style the last tile (the empty one) such that it is disabled

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,8 +2,28 @@ import React from 'react';
 import styles from './Tile.module.css';
 
 function Tile({number}) {
+
+  function canMove() {
+
+  }
+
+  function moveTile() {
+
+  }
+
+  function checkIfPlayerWins() {
+
+  }
+
+  function handleClick({target}) {
+    if (canMove(target)) {
+      moveTile(target);
+      checkIfPlayerWins();
+    }
+  }
+
   return(
-    <div className={styles.tile}>
+    <div className={number === 16 ? `${styles.tile} ${styles.disabled}`: styles.tile} onClick={handleClick}>
       {number}
     </div>
   )
