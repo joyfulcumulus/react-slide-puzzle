f46a5eca744f92d1edd584bc0681806399308a11
YY T <changiyy07@gmail.com>
2024-05-31 11:23:47 +0800

    Able to reset game using reset game button

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,8 +5,8 @@ import Tile from './Tile';
 function Grid({progress, setProgress}) {
   const GRID_SIZE = 4
   // generate initial shuffled numbers to set initial state
-  // const shuffledNumbers = Array.from({ length: GRID_SIZE ** 2 }, (_, index) => index + 1).sort(() => Math.random() - 0.5);
-  const shuffledNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,15]
+  const shuffledNumbers = Array.from({ length: GRID_SIZE ** 2 }, (_, index) => index + 1).sort(() => Math.random() - 0.5);
+  // const shuffledNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,15]
   const [tiles, setTiles] = useState(shuffledNumbers);
   const [emptyTileIndex, setEmptyTileIndex] = useState(shuffledNumbers.findIndex(element => element === 16));
 
@@ -27,7 +27,8 @@ function Grid({progress, setProgress}) {
   // reset gameboard whenever progress is changed to 0
   useEffect(() => {
     if(progress === 0) {
-      console.log("game reset!");
+      const shuffledNumbers = Array.from({ length: GRID_SIZE ** 2 }, (_, index) => index + 1).sort(() => Math.random() - 0.5);
+      setTiles(shuffledNumbers);
     }
   }, [progress])
 
