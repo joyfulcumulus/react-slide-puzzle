a779ad317b6aabe7d55464101fe2e9cde1fade15
YY T <changiyy07@gmail.com>
2024-05-31 10:45:35 +0800

    Incorporate Button component into WinAlert

--- src/components/WinAlert.js
+++ src/components/WinAlert.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './WinAlert.module.css';
+import Button from './Button';
 
 function WinAlert() {
   return(
     <div className={styles.container}>
       <div className={styles.texts}>
         <p>You Win!</p>
-        <button>Play Again</button>
+        <Button text="Play Again"/>
       </div>
     </div>
   )
